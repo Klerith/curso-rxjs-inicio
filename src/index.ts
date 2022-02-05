82cbea5db682f7afdc971f1380014aadaded0732
Lion <breakermoob@hotmail.com>
2022-02-05 18:03:32 -0500

    feat: observables and subscriptions - unsubscribe chain

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Observable, Observer } from "rxjs";
 
 const observer: Observer<any> = {
-    next: (res) => console.log(res),
+    next: (res) => console.log('num', res),
     error: (err: any) => console.warn(err),
     complete: () => console.log('Complete'),
 };
@@ -20,6 +20,12 @@ const intervalo$ = new Observable<number>(subscriber => {
         console.log('open Observable', count);
     }, 2500);
 
+    setTimeout(() => {
+        //Execute the return function immediately
+        console.log('complete');
+        subscriber.complete();
+    }, 3000);
+
     //Return apply when call unsubscribe
     return () => {
         clearInterval(intervalo);
@@ -28,21 +34,18 @@ const intervalo$ = new Observable<number>(subscriber => {
 });
 
 //Init subscriptions
-const subs1 = intervalo$.subscribe(num => {
-    console.log('Num', num);
-});
-const subs2 = intervalo$.subscribe(num => {
-    console.log('Num', num);
-});
-const subs3 = intervalo$.subscribe(num => {
-    console.log('Num', num);
-});
+const subs1 = intervalo$.subscribe(observer);
+const subs2 = intervalo$.subscribe(observer);
+const subs3 = intervalo$.subscribe(observer);
+
+subs1.add(subs2);
+subs1.add(subs3);
 
 //close subscriptions
 setTimeout(() => {
     subs1.unsubscribe();
-    subs2.unsubscribe();
-    subs3.unsubscribe();
+    // subs2.unsubscribe();
+    // subs3.unsubscribe();
     console.log('Completado timeout');
 }, 7500)
 
